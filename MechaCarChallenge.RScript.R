54567eaa61820ea0e875b67707170d02b0445c14
Amber Teetsel <amberteetsel@gmail.com>
2021-03-21 15:57:57 -0600

    Upload suspension coil PSI technical analysis

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -1,11 +1,12 @@
-# PURPOSE:
+# DELIVERABLE 1
 # Design a linear model that predicts the mpg of MechaCar prototypes
 
 # Load dplyr
 library(dplyr)
+library(tidyverse)
 
 # Import and read MechaCar_mpg as csv
-mcar <- read_csv(file="Data/MechaCar_mpg.csv")
+mcar <- read.csv(file="Data/MechaCar_mpg.csv", check.names = F, stringsAsFactors = F)
 
 # Perform linear regression using all 6 variables
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + 
@@ -15,3 +16,22 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle +
 # Using the summary() function
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + 
              ground_clearance + AWD, data = mcar))
+
+# DELIVERABLE 2
+# Visualize the suspension coil's PSI continuous variable across all lots
+# Summary statistics on PSI metrics for each lot
+
+# Import and read Suspension_Coil as csv
+sus_coil <- read.csv(file="Data/Suspension_Coil.csv", check.names = F, stringsAsFactors = F)
+
+# Create summary dataframe to get mean, median, variance, and stdev of PSI
+total_summary <- sus_coil %>% summarize(Mean_PSI=mean(PSI),
+                                        Median_PSI=median(PSI),
+                                        Variance=var(PSI),
+                                        SD=sd(PSI))
+
+# Create summary dataframe by lot
+lot_summary <- sus_coil %>% group_by(Manufacturing_Lot) %>%
+  summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI),
+            Variance=var(PSI), SD=sd(PSI), .groups = "keep")
+                                        
